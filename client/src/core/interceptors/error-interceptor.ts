6fede4219be82636a97d24d36721a6374f7a82b8
kleo jahollari <kleojahollai43@gmail.com>
2025-09-13 13:46:19 +0200

    Section 12

--- client/src/core/interceptors/error-interceptor.ts
+++ client/src/core/interceptors/error-interceptor.ts
@@ -9,7 +9,7 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
 
   return next(req).pipe(
-    catchError(error => {
+    catchError((error) => {
       if (error) {
         switch (error.status) {
           case 400:
@@ -17,23 +17,25 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
               const modelStateErrors = [];
               for (const key in error.error.errors) {
                 if (error.error.errors[key]) {
-                  modelStateErrors.push(error.error.errors[key])
+                  modelStateErrors.push(error.error.errors[key]);
                 }
               }
               throw modelStateErrors.flat();
             } else {
-              toast.error(error.error)
+              toast.error(error.error);
             }
             break;
           case 401:
             toast.error('Unauthorized');
             break;
           case 404:
-            router.navigateByUrl('/not-found')
+            router.navigateByUrl('/not-found');
             break;
           case 500:
-            const navigationExtras: NavigationExtras = {state: {error: error.error}}
-            router.navigateByUrl('/server-error', navigationExtras)
+            const navigationExtras: NavigationExtras = {
+              state: { error: error.error },
+            };
+            router.navigateByUrl('/server-error', navigationExtras);
             break;
           default:
             toast.error('Something went wrong');
@@ -43,5 +45,5 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
 
       throw error;
     })
-  )
+  );
 };
--- client/src/shared/text-input/text-input.ts
+++ client/src/shared/text-input/text-input.ts
@@ -15,6 +15,7 @@ import {
 export class TextInput implements ControlValueAccessor {
   label = input<string>('');
   type = input<string>('text');
+  maxDate = input<string>('');
 
   constructor(@Self() public ngControl: NgControl) {
     this.ngControl.valueAccessor = this; // dmth q klasa TextInput eshte e barabarte me this.ngControl.valueAccessor kjo dmnth kjo this
