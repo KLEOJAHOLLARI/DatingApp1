8d20d5b49a09ba9ce8022c403d7e8243bc82e70c
kleo jahollari <kleojahollai43@gmail.com>
2025-09-18 18:44:53 +0200

    Section 14

--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,19 +1,22 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { of } from 'rxjs';
+import { LikesServices } from './likes-services';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InitService {
   private accountService = inject(AccountService);
+  private likesService = inject(LikesServices);
 
   init() {
     const userString = localStorage.getItem('user');
     if (!userString) return of(null);
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
+    this.likesService.getLikesIds();
 
-    return of(null)
+    return of(null);
   }
 }
--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -1,14 +1,34 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../../types/member';
 import { RouterLink } from '@angular/router';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { LikesServices } from '../../../core/services/likes-services';
 
 @Component({
   selector: 'app-member-card',
   imports: [RouterLink, AgePipe],
   templateUrl: './member-card.html',
-  styleUrl: './member-card.css'
+  styleUrl: './member-card.css',
 })
 export class MemberCard {
+  private likeService = inject(LikesServices);
   member = input.required<Member>();
+  protected hasLiked = computed(() =>
+    this.likeService.likeIds().includes(this.member().id)
+  );
+
+  toggleLike(event: Event) {
+    event.stopPropagation();
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likeService.likeIds.update((ids) =>
+            ids.filter((x) => x !== this.member().id)
+          );
+        } else {
+          this.likeService.likeIds.update((ids) => [...ids, this.member().id]);
+        }
+      },
+    });
+  }
 }
